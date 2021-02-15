20b85ed2e1c0e6ca33116547d4f234bb7524d100
nkuek <nkuek1@gmail.com>
2021-02-15 02:24:26 -0500

    completed all sample form specs

--- src/components/SampleSurvey.js
+++ src/components/SampleSurvey.js
@@ -1,33 +1,62 @@
-import Form, { Col } from 'react-bootstrap/Form';
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { useState, useEffect, Fragment } from 'react';
 
 const SampleSurvey = ({ data }) => {
+    // Set Form States
     const [reason, setReason] = useState('');
     const [plan, setPlan] = useState('');
     const [whyOrWhyNot, setWhyOrWhyNot] = useState('');
     const [firstName, setFirstName] = useState('');
     const [email, setEmail] = useState('');
     const [addtionalFeedback, setAdditionalFeedback] = useState('');
+    const [isSubmitted, setIsSubmitted] = useState(false);
     const [errors, setErrors] = useState([]);
-    useEffect(() => {
-        console.log('reason', reason);
-        console.log('plan', plan);
+
+    // Creation of form submission event handler
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        // Will scroll back to the top of the page if there are errors
+        window.scrollTo(0, 0);
+
+        // Form validation
         const errors = [];
         if (!reason || !plan)
             errors.push('Please fill out all required fields');
-    }, [reason, plan, whyOrWhyNot, firstName, email, addtionalFeedback]);
+        if (whyOrWhyNot.length === 0)
+            errors.push('Please answer why or why not');
+        setErrors(errors);
+
+        // Set isSubmitted state to true if there are no errors
+        if (errors.length === 0) setIsSubmitted(true);
+    };
+
+    // Renders thank you message instead of the form if it is submitted without errors
+    if (isSubmitted) return <h2 className="header">{data.thankyou}</h2>;
+
+    // Render Form
     return (
         <div>
             <h2 className="header">{data.name}</h2>
-            <Form className="form">
+            <Form className="form" onSubmit={onSubmit}>
+                {/* If there are any errors, render list of errors */}
+                <ul className="errors">
+                    {errors &&
+                        errors.map((error, idx) => {
+                            return <li key={idx}>{error}</li>;
+                        })}
+                </ul>
+                {/* Render mcq as radio button questions */}
                 {data.questions.map((question, idx) => {
                     if (question.type === 'mcq') {
                         return (
                             <div className="mb-3" key={idx}>
                                 <Form.Label as="legend">
-                                    {question.stem}
+                                    {question.stem} *
                                 </Form.Label>
+
+                                {/* Creation of form elements for every question in the sample json */}
                                 {question.options.map((option, i) => (
                                     <Form.Check
                                         key={i}
@@ -36,6 +65,7 @@ const SampleSurvey = ({ data }) => {
                                         type="radio"
                                         value={option.value}
                                         name={idx}
+                                        // Conditional event listener for react state depending on the question
                                         onChange={(e) =>
                                             question.stem.includes('reason')
                                                 ? setReason(e.target.value)
@@ -45,13 +75,16 @@ const SampleSurvey = ({ data }) => {
                                 ))}
                             </div>
                         );
-                    } else if (question.type === 'cr') {
+                    }
+                    // Render cr questions
+                    else if (question.type === 'cr') {
                         return (
                             <div className="mb-3" key={idx}>
                                 <Form.Label as="legend">
                                     {question.stem}
                                 </Form.Label>
                                 <Form.Control
+                                    // Set various react state to the input value depending on question
                                     onChange={(e) =>
                                         question.stem.includes('Why')
                                             ? setWhyOrWhyNot(e.target.value)
@@ -63,18 +96,21 @@ const SampleSurvey = ({ data }) => {
                                                   e.target.value
                                               )
                                     }
+                                    // If question has instructions, renders them as placeholder text
                                     placeholder={
                                         question.instructions
                                             ? question.instructions
                                             : ''
                                     }
+                                    // If the question specifies lines > 1, renders as textarea
+                                    // else renders as plain input field
                                     as={
                                         question.lines > 1
                                             ? 'textarea'
                                             : 'input'
                                     }
+                                    // Expands textarea to the number of lines in the question
                                     rows={question.lines}
-                                    required={!question.optional}
                                 ></Form.Control>
                             </div>
                         );
